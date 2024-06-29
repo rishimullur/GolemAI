c243ceaae3b5dd1166ae21ce7323aec72c864e41
Michael Goh <github@jwgoh.com>
2024-06-29 14:09:35 -0700

    completed context

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from helper.context import save, retrieve
+from schemas.schemas import UserBase, ChatBase, MessageBase
 import uvicorn
 
 app = FastAPI()
@@ -12,10 +14,24 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/chat")
-def chat():
-    print("Chat input recived")
-    # return {"chat": "chat"}
+
+#TODO: Mike
+
+@app.post("/chat")
+def save_chat(user_base: UserBase, chat_base: ChatBase, message_base: MessageBase):
+    try:
+        save(user_base, chat_base, message_base)
+        return HTTPException(status_code=200, detail={"status": "success", "message": "Chat saved successfully"})
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"status": "error", "message": str(e)})
+
+
+@app.post("/process")
+def process():
+    ...
+
+#END TD
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8005)
