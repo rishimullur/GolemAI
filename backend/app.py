9460d60ef9a8163a483a74f1bc57c2259086bccd
Michael Goh <github@jwgoh.com>
2024-06-29 13:59:52 -0700

    api endpoints

--- backend/app.py
+++ backend/app.py
@@ -11,5 +11,19 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
+
+#TODO: Mike
+
+@app.post("/chat")
+def save_chat():
+    ...
+
+@app.post("/process")
+def process():
+    ...
+
+#END TD
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8005)
