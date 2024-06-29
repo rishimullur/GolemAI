4c1fd00a9763bf6d039482a5cf2df8d904a906f4
“Rishi” <rishiraj.mullur1997@gmail.com>
2024-06-29 14:23:55 -0700

    api stuff with merge conflict

--- backend/app.py
+++ backend/app.py
@@ -13,10 +13,5 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/chat")
-def chat():
-    llama_chat("What is the capital of France?")
-    # return {"chat": "chat"}
-
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8005)
