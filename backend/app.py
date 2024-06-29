504a8c0ce4b991a7c4561855a00cb0e25460dd35
“Rishi” <rishiraj.mullur1997@gmail.com>
2024-06-29 12:38:45 -0700

    schema

--- backend/app.py
+++ backend/app.py
@@ -11,5 +11,11 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
+@app.get("/chat")
+def chat():
+    print("Chat input recived")
+    # return {"chat": "chat"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8005)
